3989bb7cad38f59c3d98f535dd96b07f2cdee775
Neelansh Sahai <neelanshsahai@google.com>
2023-06-12 23:35:17 +0530

    Changes after Class 4

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.calculatorapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
+// TODO 0: Clone the calc repo
+// TODO 1: Create a new branch <name_calc>
+// TODO 2: Push all your code to this new branch
+// TODO 3: Share the link of your branch with me (on Discord or on WA)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
